393a3d398685b819a98b1dac0d12d1dc6cf407f1
SterlingKov <sterling.kovash@gmail.com>
2022-01-18 21:40:28 -0600

    testing

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,4 @@
 print("hello world")
 print("hello again")
 print("I FINALLY FIGURED OUT HOW TO DO THIS STUPID THING AFTER 2 HOURS EVEN THOUGH I DIDNT DO IT THE 'ACTUAL' WAY")
+print("trying to figure it out")
